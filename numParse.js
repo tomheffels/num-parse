02144636fae8eb76c8ba6f540ec8d850cab27280
Tom Heffels <tomheffels@hotmail.com>
2019-07-08 18:48:34 +0200

    Initial commit: numParse function

---
+++ numParse.js
@@ -0,0 +1,23 @@
+// Parses string for number and returns it as an integer.
+const numParse = (string) => {
+  const noCommaString = string.replace(/,/g, '.')
+  const firstNum = noCommaString.match(/\d/)
+  const firstNumIndex = noCommaString.indexOf(firstNum)
+  const croppedString = noCommaString.slice(firstNumIndex)
+  const numFloat = firstNum && parseFloat(croppedString)
+  const numMultiplier = croppedString.split(numFloat)[1][0].toUpperCase()
+  switch(numMultiplier) {
+    case 'K':
+      return numFloat * 1000
+    case 'M':
+      return numFloat * 1000000
+    case 'B':
+      return numFloat * 1000000000
+    case 'T':
+      return numFloat * 1000000000000
+    default:
+      return Math.round(numFloat)
+  }
+}
+
+module.exports = numParse
